69654e6bfdc85be0e2e4a2351603135b2067e50a
RuslanShap <shapoval.websites@gmail.com>
2020-01-23 14:13:04 +0200

    adding output of the completed tasks and displaying a message

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,8 +4,11 @@ var todo = document.querySelector('.todo');
 var todoNew = todo.querySelector('.todo__new');
 var todoListCurrent = todo.querySelector('.todo__list--current');
 var todoListDone = todo.querySelector('.todo__list--done');
+var todoCount = todo.querySelector('.todo__count');
+var todoMessage = todo.querySelector('.todo__message');
 
 todoNew.addEventListener('change', addTask);
+todoNew.addEventListener('change', addCount);
 
 function addTask() {
   var value = todoNew.value;
@@ -34,6 +37,9 @@ function createTask(value) {
 
   todoBtnDel.addEventListener('click', delTask);
   todoCheck.addEventListener('click', checkTask);
+
+  todoBtnDel.addEventListener('click', addCount);
+  todoCheck.addEventListener('click', addCount);
 }
 
 function delTask() {
@@ -46,5 +52,52 @@ function checkTask() {
     todoListDone.append(item);
   } else {
     todoListCurrent.append(item);
+  }
+}
+
+function addCount() {
+  var currentItems = todo.querySelectorAll('.todo__item');
+
+  if (currentItems.length) {
+    var doneItems = todoListDone.querySelectorAll('.todo__item');
+    var totalAmount = currentItems.length;
+    var totalDone = doneItems.length;
+    var percent = totalDone / totalAmount;
+
+    todoCount.textContent = totalDone + '/' + totalAmount;
+
+    showMessage(percent);
+
+  } else {
+    todoCount.textContent = '';
+    todoMessage.textContent = '';
+  }
+}
+
+function showMessage(percent) {
+  switch (true) {
+    case (percent <= 0):
+      todoMessage.textContent = 'Начни наконец!';
+      break;
+
+    case (percent <= 0.25):
+      todoMessage.textContent = 'Лучше чем ничего.';
+      break;
+    case (percent <= 0.5):
+      todoMessage.textContent = 'Ну такое.';
+      break;
+    case (percent <= 0.75):
+      todoMessage.textContent = 'Ты на верном пути.';
+      break;
+    case (percent < 1):
+      todoMessage.textContent = 'Не думал, что ты это сможешь.';
+      break;
+    case (percent === 1):
+      todoMessage.textContent = 'Мог бы и быстрее!';
+      break;
+
+    default:
+      todoMessage.textContent = 'Сверхчеловек?!';
+      break;
   }
 }
